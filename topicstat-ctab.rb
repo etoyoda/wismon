d34eb7c24b8a2ee6a560ac1c81a9d982c5f7797a
TOYODA Eizi <toyoda.eizi@gmail.com>
2025-10-28 00:05:59 +0000

    stats added

--- topicstat-ctab.rb
+++ topicstat-ctab.rb
@@ -1,41 +1,70 @@
 #!/usr/bin/ruby
 
+class Stat
+
+  def initialize
+    @stat1=Hash.new(0)
+    @stat2=Hash.new(0)
+    @sum=0.0
+  end
+
+  def register metric,cat1,cat2
+    return false if metric.nan?
+    @stat1[cat1]+=metric
+    @stat2[cat2]+=metric
+    @sum+=metric
+  end
+
+  def format7 n
+    if n<999.5 then format("%6.3f",n)
+      elsif n<999.5e3 then format("%6.2fk",n*0.001)
+      elsif n<999.5e6 then format("%6.2fM",n*1.0e-6)
+      elsif n<999.5e9 then format("%6.2fG",n*1.0e-9)
+      else format("%6.2fT",n*1.0e-12)
+      end
+  end
+
+  def report kwd
+    puts "= "+kwd
+    @stat1.keys.sort{|a,b| @stat1[b]<=>@stat1[a] }.each{|cat1|
+      n=@stat1[cat1]
+      r=100.0*n/@sum
+      printf "%7s\t%5.2f%%\t%s\n", format7(n), r, cat1
+    }
+  end
+
+end
+
 cidstat=Hash.new(0)
 catstat=Hash.new(0)
-cat1nstat=Hash.new(0)
-cat1zstat=Hash.new(0)
+nstat=Stat.new
+zstat=Stat.new
 
 for line in ARGF
   n,z,tc=line.strip.split(/,/,3)
-  n=n.to_i
-  z=z.to_f
+  n=n.to_f
+  z=z.to_f*n*1000.0
   cid,dm,pol,wx,cat=tc.split(/\//,5)
+  cat='(gts)' if tc=='(gts)'
   cidstat[cid]+=1 if cid
   catstat[cat]+=1 if cat
   cat1 = if cat then cat.split(/\//,2)[0]
-    elsif tc=='(gts)' then '(gts)'
-    else nil
-    end
-  cat1nstat[cat1]+=n if cat1
-  cat1zstat[cat1]+=z if cat1 and not z.nan?
+    else nil end
+  cat2 = if cat then cat.split(/\//,3)[0,2].join('/')
+    else nil end
+  nstat.register(n,cat1,cat2) if cat
+  zstat.register(z,cat1,cat2) if cat
 end
 
-puts "= centre_id, #topics for"
+puts "= n topics for centreid"
 for cid in cidstat.keys.sort
   printf("%s\t%s\n", cid, cidstat[cid])
 end
 
-puts "= category, #topics for"
+puts "= n topics for category"
 for cat in catstat.keys.sort
   printf("%s\t%s\n", cat, catstat[cat])
 end
 
-puts "= category1, #messages for"
-sum=0
-cat1nstat.values.each{|n| sum+=n}
-for cat in cat1nstat.keys.sort{|a,b|cat1nstat[b]<=>cat1nstat[a]}
-  printf("%7u\t%6.1f\t%s\n", cat1nstat[cat], cat1nstat[cat]*100.0/sum, cat)
-end
-
-
-
+nstat.report("num messages")
+zstat.report("size")

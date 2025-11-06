923b88ef171ee0894c7490b52e12764fad23c99d
TOYODA Eizi <toyoda.eizi@gmail.com>
2025-11-06 00:21:56 +0000

    wisdev のトピックリストも取得することに

--- run-topicstat.sh
+++ run-topicstat.sh
@@ -12,12 +12,14 @@ ymd=$2
 test -d $ym || mkdir $ym
 cd $ym
 ruby ${base}/wnm-topicstat.rb jmagc 2> topics.log > topics${ymd}.txt
+ruby ${base}/topicstat-ctab.rb topics${ymd}.txt 2> ctab.log > ctab${ymd}.txt
 ruby ${base}/wnm-topicstat.rb jmagc -gc=jp-jma-global-cache 2> topicsj.log \
  > topicsj${ymd}.txt
-ruby ${base}/topicstat-ctab.rb topics$ymd.txt 2> ctab.log > ctab$ymd.txt
-ruby ${base}/topicstat-ctab.rb topicsj$ymd.txt 2> ctabj.log > ctabj$ymd.txt
+ruby ${base}/topicstat-ctab.rb topicsj${ymd}.txt 2> ctabj.log > ctabj${ymd}.txt
+ruby ${base}/wnm-topicstat.rb devgc 2> topicsd.log > topicsd${ymd}.txt
+ruby ${base}/topicstat-ctab.rb topicsd${ymd}.txt 2> ctabd.log > ctabd${ymd}.txt
 
-cd ..
+cd /nwp/m1
 
 prev=''
 if test -f gtshist-jmagc.txt

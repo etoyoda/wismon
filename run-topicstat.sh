35d0c34c15ebc0a278aa26df31d7d3ea0bc88a4d
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-08-13 23:19:30 +0000

    new map convobs2.png added

--- run-topicstat.sh
+++ run-topicstat.sh
@@ -40,6 +40,7 @@ rm -f z.convobs.txt
 cd /nwp/m1
 CONVOBS=convobs.txt
 
+##--- BEGIN DRAWING
 if test -x /usr/bin/gmt
 then
   YPS=y.convobs.ps
@@ -53,23 +54,57 @@ then
   PROJ="-JQ0/5i"
   # DATE=$(awk '{print $2}' $CONVOBS | sort -r | head -10 | tail -1)
   DATE=$(TZ=UTC+0 date +%Y%m%dT0000 --date '24 hours ago')
+  rm -f $YPS $YTXT $YTXT2 
+  : map 1 - SYNOP and TEMP
   gmt pscoast $REGION $PROJ -B30g30 -Dc -A5000 -W0.25p -N1/0.25p -P -K > $YPS
   awk '($2 >= "'${DATE}'" && $8 ~ /synop/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT
   gmt psxy $REGION $PROJ -Sc2p -Gorange -W0.25p -O -K < $YTXT >> $YPS
-  awk '($2 >= "'${DATE}'" && $8 ~ /temp/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
-  gmt psxy $REGION $PROJ -Sc3p -W0.5p,blue -O -K < $YTXT2 >> $YPS
-  gmt pslegend $REGION $PROJ -Dg-180/-45+w1.1i+jTL+o0.1i -F+gwhite+p0.25p+r3p -O >> $YPS <<ENDLEGEND
+  awk '(!/DROP/ && $2 >= "'${DATE}'" && $8 ~ /temp/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
+  gmt psxy $REGION $PROJ -St3p -W0.5p,blue -O -K < $YTXT2 >> $YPS
+  awk '(/DROP/ && $2 >= "'${DATE}'" && $8 ~ /temp/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
+  gmt psxy $REGION $PROJ -Si3p -W0.5p,cyan -O -K < $YTXT2 >> $YPS
+  gmt pslegend $REGION $PROJ -Dg-170/-34+w1.0i+jTL+o0.1i -F+gwhite+p0.25p+r2p -O >> $YPS <<ENDLEGEND
 H 6p,Helvetica-Bold WIS2 Data Coverage
 G 0p
 H 6p,Helvetica-Bold ${DATE}Z/PT24
 G 1p
-S 0.05i c 2p orange 0.25p 0.2i SYNOP
-S 0.05i c 3p blue 0.5p 0.2i TEMP
+S 0.05i c 2p orange 0.25p 0.1i SYNOP
+G -6.5p
+S 0.50i t 3p - 0.5p,blue 0.55i TEMP
+ENDLEGEND
+  gmt psconvert $YPS -A+m0.2c -Tg -P
+  rm -f $YPS $YTXT $YTXT2
+  mv -f y.convobs.png convobs.png
+  : map 2 - SHIP and BUOY
+  gmt pscoast $REGION $PROJ -B30g30 -Dc -A5000 -W0.25p -N1/0.25p -P -K > $YPS
+  awk '($2 >= "'${DATE}'" && $8 ~ /\/ship/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT
+  gmt psxy $REGION $PROJ -Sc2p -Gorange -W0.25p -O -K < $YTXT >> $YPS
+  awk '($2 >= "'${DATE}'" && $8 ~ /\/wind-profile/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT
+  gmt psxy $REGION $PROJ -Ss3p -Gyellow -W0.25p,red -O -K < $YTXT >> $YPS
+  awk '($2 >= "'${DATE}'" && $8 ~ /drifting-buoy/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
+  gmt psxy $REGION $PROJ -Sd3p -W0.4p,cyan -O -K < $YTXT2 >> $YPS
+  awk '($2 >= "'${DATE}'" && $8 ~ /moored-buoy/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
+  gmt psxy $REGION $PROJ -Si3p -W0.4p,blue -O -K < $YTXT2 >> $YPS
+  awk '($2 >= "'${DATE}'" && $8 ~ /wave-buoy/ && $7==$7+0 && $6==$6+0){print $7, $6}' $CONVOBS > $YTXT2
+  gmt psxy $REGION $PROJ -Sa3p -W0.4p,blue -O -K < $YTXT2 >> $YPS
+  gmt pslegend $REGION $PROJ -Dg-180/-65+w2.6i+jTL+o0.1i -F+gwhite+p0.25p+r3p -O >> $YPS <<ENDLEGEND
+H 6p,Helvetica-Bold WIS2 Data Coverage ${DATE}Z/PT24
+G 0p
+S 0.05i c 2p orange 0.20p 0.1i SHIP
+G -6.5p
+S 0.45i d 3p - 0.20p,cyan 0.5i DRIFT
+G -6.5p
+S 0.85i i 3p - 0.20p,blue 0.9i MOORED
+G -6.5p
+S 1.45i a 3p - 0.20p,blue 1.5i WAVE
+G -6.5p
+S 1.85i s 3p yellow 0.20p,red 1.9i WPROF
 ENDLEGEND
   gmt psconvert $YPS -A+m0.2c -Tg -P
   rm -f $YPS $YTXT $YTXT2 gmt.conf gmt.history
-  mv -f y.convobs.png convobs.png
+  mv -f y.convobs.png convobs2.png
 fi
+##--- END DRAWING
 
 if [ -f /nwp/bin/send_png_mail.rb ] && [ -f convobs.png ]; then
   ruby /nwp/bin/send_png_mail.rb convobs.png

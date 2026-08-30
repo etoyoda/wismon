29187c069395fbb09f2421cdbabb87c0516b4a28
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-08-30 23:16:02 +0000

    working on #21 - deploying run-convobs.sh

--- Deploy.sh
+++ Deploy.sh
@@ -39,7 +39,7 @@ else
 fi
 
 for p in wnmsaver.rb wnm-topicstat.rb run-topicstat.sh topicstat-ctab.rb \
-  wnm-gtshist.rb wnm-convobs.rb convobs-merge.rb wnm-obscache.rb
+  wnm-gtshist.rb wnm-convobs.rb convobs-merge.rb wnm-obscache.rb run-convobs.sh
 do
   test -f $p
   sudo install $p /usr/local/bin/
